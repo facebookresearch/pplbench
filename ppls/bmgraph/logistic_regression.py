b393adb067943dc2fa51a0394f5daf3a1efbae6d
Kinjal Shah <kshah97@fb.com>
2020-06-24 17:51:26 -0700

    Have all ppl implementations inherit from a standard PPL class (#119)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/BeanMachine/pull/119
    
    To enforce the implementation of the ```obtain_posterior ``` function, and to have better code quality in general, all ppl implementations now have to inherit from an abstract PPL class.
    
    Reviewed By: dnoursi
    
    Differential Revision: D20378788
    
    fbshipit-source-id: c3ead1e4bf8acd5eab9888b0c7112b446b3e5b8f

--- ppls/bmgraph/logistic_regression.py
+++ ppls/bmgraph/logistic_regression.py
@@ -11,81 +11,90 @@ from typing import Any, Dict, List, Tuple
 import beanmachine.graph as bmgraph
 import numpy as np
 
+from ..pplbench_ppl import PPLBenchPPL
 
-def obtain_posterior(
-    data_train: Tuple[Any, Any], args_dict: Dict, model
-) -> Tuple[List, Dict]:
-    """
-    Beanmachine impmementation of logisitc regression model.
 
-    :param data_train: tuple of np.ndarray (x_train, y_train)
-    :param args_dict: a dict of model arguments
-    :returns: samples_beanmachine(dict): posterior samples of all parameters
-    :returns: timing_info(dict): compile_time, inference_time
-    """
-    x_train, y_train = data_train
-    K, N = x_train.shape  # K = num features, N = num observations
-    assert y_train.shape == (N,)
-    assert model is None
-    num_samples = int(args_dict["num_samples"])
-    alpha_scale, beta_scale, beta_loc, _ = args_dict["model_args"]
-    # compile the model
-    compile_start = time.time()
-    g = bmgraph.Graph()
-    # alpha ~ Normal(0, alpha_scale)
-    zero = g.add_constant(0.0)
-    alpha_scale = g.add_constant_pos_real(float(alpha_scale))
-    alpha_prior = g.add_distribution(
-        bmgraph.DistributionType.NORMAL, bmgraph.AtomicType.REAL, [zero, alpha_scale]
-    )
-    alpha = g.add_operator(bmgraph.OperatorType.SAMPLE, [alpha_prior])
-    # beta_j ~ Normal(beta_loc, beta_scale) for j in range(K)
-    beta_scale = g.add_constant_pos_real(float(beta_scale))
-    beta_loc = g.add_constant(float(beta_loc))
-    beta_prior = g.add_distribution(
-        bmgraph.DistributionType.NORMAL, bmgraph.AtomicType.REAL, [beta_loc, beta_scale]
-    )
-    beta = [g.add_operator(bmgraph.OperatorType.SAMPLE, [beta_prior]) for _ in range(K)]
-    # yhat_i = alpha + sum(x_j_i * beta_j for j in range(K))
-    for i in range(N):
-        sum_i = []
-        for j in range(K):
-            x_j_i = g.add_constant(float(x_train[j, i]))
-            x_j_i_times_beta_j = g.add_operator(
-                bmgraph.OperatorType.MULTIPLY, [x_j_i, beta[j]]
-            )
-            sum_i.append(x_j_i_times_beta_j)
-        sum_i.append(alpha)
-        yhat_i = g.add_operator(bmgraph.OperatorType.ADD, sum_i)
-        # y_i ~ Bernoulli(logit = yhat_i)
-        bernoulli_i = g.add_distribution(
-            bmgraph.DistributionType.BERNOULLI_LOGIT,
-            bmgraph.AtomicType.BOOLEAN,
-            [yhat_i],
+class LogisticRegression(PPLBenchPPL):
+    def obtain_posterior(
+        self, data_train: Tuple[Any, Any], args_dict: Dict, model
+    ) -> Tuple[List, Dict]:
+        """
+        Beanmachine impmementation of logisitc regression model.
+
+        :param data_train: tuple of np.ndarray (x_train, y_train)
+        :param args_dict: a dict of model arguments
+        :returns: samples_beanmachine(dict): posterior samples of all parameters
+        :returns: timing_info(dict): compile_time, inference_time
+        """
+        x_train, y_train = data_train
+        K, N = x_train.shape  # K = num features, N = num observations
+        assert y_train.shape == (N,)
+        assert model is None
+        num_samples = int(args_dict["num_samples"])
+        alpha_scale, beta_scale, beta_loc, _ = args_dict["model_args"]
+        # compile the model
+        compile_start = time.time()
+        g = bmgraph.Graph()
+        # alpha ~ Normal(0, alpha_scale)
+        zero = g.add_constant(0.0)
+        alpha_scale = g.add_constant_pos_real(float(alpha_scale))
+        alpha_prior = g.add_distribution(
+            bmgraph.DistributionType.NORMAL,
+            bmgraph.AtomicType.REAL,
+            [zero, alpha_scale],
         )
-        y_i = g.add_operator(bmgraph.OperatorType.SAMPLE, [bernoulli_i])
-        g.observe(y_i, bool(y_train[i]))
-    # We want P (alpha, beta_j for j in range(K) | x_i, y_i  for i in range(N))
-    g.query(alpha)
-    for j in range(K):
-        g.query(beta[j])
-    # we will include the inference time for one sample into the compile time
-    # and subtract this from inference time later
-    infer_setup_start = time.time()
-    g.infer(1, bmgraph.InferenceType.NMC)
-    infer_setup_time = time.time() - infer_setup_start
-    compile_time = time.time() - compile_start
-    infer_start = time.time()
-    samples = g.infer(
-        num_samples, bmgraph.InferenceType.NMC, int(time.time() * 1000) % 2 ** 31
-    )
-    infer_time = time.time() - infer_start - infer_setup_time
-    timing_info = {"compile_time": compile_time, "inference_time": infer_time}
-    print(f"bmgraph inference time {infer_time:.1f}")
-    sample_dicts = []
-    for sample in samples:
-        dict_ = {}
-        dict_["alpha"] = sample[0]
-        dict_["beta"] = np.array(sample[1:])
-        sample_dicts.append(dict_)
-    return sample_dicts, timing_info
+        alpha = g.add_operator(bmgraph.OperatorType.SAMPLE, [alpha_prior])
+        # beta_j ~ Normal(beta_loc, beta_scale) for j in range(K)
+        beta_scale = g.add_constant_pos_real(float(beta_scale))
+        beta_loc = g.add_constant(float(beta_loc))
+        beta_prior = g.add_distribution(
+            bmgraph.DistributionType.NORMAL,
+            bmgraph.AtomicType.REAL,
+            [beta_loc, beta_scale],
+        )
+        beta = [
+            g.add_operator(bmgraph.OperatorType.SAMPLE, [beta_prior]) for _ in range(K)
+        ]
+        # yhat_i = alpha + sum(x_j_i * beta_j for j in range(K))
+        for i in range(N):
+            sum_i = []
+            for j in range(K):
+                x_j_i = g.add_constant(float(x_train[j, i]))
+                x_j_i_times_beta_j = g.add_operator(
+                    bmgraph.OperatorType.MULTIPLY, [x_j_i, beta[j]]
+                )
+                sum_i.append(x_j_i_times_beta_j)
+            sum_i.append(alpha)
+            yhat_i = g.add_operator(bmgraph.OperatorType.ADD, sum_i)
+            # y_i ~ Bernoulli(logit = yhat_i)
+            bernoulli_i = g.add_distribution(
+                bmgraph.DistributionType.BERNOULLI_LOGIT,
+                bmgraph.AtomicType.BOOLEAN,
+                [yhat_i],
+            )
+            y_i = g.add_operator(bmgraph.OperatorType.SAMPLE, [bernoulli_i])
+            g.observe(y_i, bool(y_train[i]))
+        # We want P (alpha, beta_j for j in range(K) | x_i, y_i  for i in range(N))
+        g.query(alpha)
+        for j in range(K):
+            g.query(beta[j])
+        # we will include the inference time for one sample into the compile time
+        # and subtract this from inference time later
+        infer_setup_start = time.time()
+        g.infer(1, bmgraph.InferenceType.NMC)
+        infer_setup_time = time.time() - infer_setup_start
+        compile_time = time.time() - compile_start
+        infer_start = time.time()
+        samples = g.infer(
+            num_samples, bmgraph.InferenceType.NMC, int(time.time() * 1000) % 2 ** 31
+        )
+        infer_time = time.time() - infer_start - infer_setup_time
+        timing_info = {"compile_time": compile_time, "inference_time": infer_time}
+        print(f"bmgraph inference time {infer_time:.1f}")
+        sample_dicts = []
+        for sample in samples:
+            dict_ = {}
+            dict_["alpha"] = sample[0]
+            dict_["beta"] = np.array(sample[1:])
+            sample_dicts.append(dict_)
+        return sample_dicts, timing_info
--- ppls/stan/n_schools.py
+++ ppls/stan/n_schools.py
@@ -5,6 +5,8 @@ import time
 import numpy as np
 import pystan
 
+from ..pplbench_ppl import PPLBenchPPL
+
 
 CODE = """
 data {
@@ -70,157 +72,154 @@ model {
 """
 
 
-# define factorize
-def _factorize(random_effect, data):
-    combined = (
-        data[random_effect]
-        .apply(lambda row: "____".join(row.values.astype(str)), axis=1)
-        .values
-    )
-    lvl2num = {}
-    i = 1
-    for val in combined:
-        if val not in lvl2num:
-            lvl2num[val] = i
-            i += 1
-    num2lvl = {i: v for v, i in lvl2num.items()}
-    random_effect_level_map = {"num2lvl": num2lvl, "lvl2num": lvl2num}
-    return random_effect_level_map
+class NSchools(PPLBenchPPL):
+    # define factorize
+    def _factorize(self, random_effect, data):
+        combined = (
+            data[random_effect]
+            .apply(lambda row: "____".join(row.values.astype(str)), axis=1)
+            .values
+        )
+        lvl2num = {}
+        i = 1
+        for val in combined:
+            if val not in lvl2num:
+                lvl2num[val] = i
+                i += 1
+        num2lvl = {i: v for v, i in lvl2num.items()}
+        random_effect_level_map = {"num2lvl": num2lvl, "lvl2num": lvl2num}
+        return random_effect_level_map
 
-
-# get _get_n_levels
-def _get_n_levels(data):
-    ns = np.array([], dtype=int)
-    for random_effect in [["state", "district"], ["type"]]:
-        for i in range(len(random_effect), 0, -1):
-            curr_lvls = random_effect[:i]
-            lvl_map = _factorize(curr_lvls, data)
-            lvl2num = lvl_map["lvl2num"]
-            ns = np.append(ns, len(lvl2num))
-    return ns
-
-
-# define mapping
-def _get_mapping(data):
-    mapping = np.empty((0, data.shape[0]), dtype=int)
-    for random_effect in [["state", "district"], ["type"]]:
-        for i in range(len(random_effect), 0, -1):
-            curr_lvls = random_effect[:i]
-            lvl_map = _factorize(curr_lvls, data)
-            lvl2num = lvl_map["lvl2num"]
-            combined = (
-                data[curr_lvls]
-                .apply(lambda row: "____".join(row.values.astype(str)), axis=1)
-                .values
-            )
-            curr_map = np.array([lvl2num[x] for x in combined])
-            mapping = np.append(mapping, [curr_map], axis=0)
-
-    return mapping.T
-
-
-# define ranges
-def _get_ranges(data):
-    n_levels = _get_n_levels(data)
-    cumsum_ns = np.cumsum(n_levels, dtype=int)
-    tmp = np.concatenate((cumsum_ns, np.array([0], dtype=int)))
-    start = np.concatenate(
-        (np.array([1], dtype=int), 1 + cumsum_ns[:-1], np.array([0], dtype=int))
-    )
-    ranges = np.array((start, tmp)).T
-    return ranges
-
-
-def obtain_posterior(data_train, args_dict, model=None):
-    """
-    Stan impmementation of n-school model.
-
-    Inputs:
-    - data_train
-    - args_dict: a dict of model arguments
-    Returns:
-    - samples_stan(dict): posterior samples of all parameters
-    - timing_info(dict): compile_time, inference_time
-    """
-    global CODE
-
-    # mapping
-    mapping = _get_mapping(data_train)
-
-    # ranges
-    ranges = _get_ranges(data_train)
-
-    # n_levels
-    n_levels = _get_n_levels(data_train)
-
-    # define sum_ns
-    sum_ns = np.sum(n_levels)
-
-    # generating stan mapping to state/district/type values!
-    re_map_to_stan_params = {}
-    for i in range(data_train.shape[0]):
-        re_q_position = 0
+    # get _get_n_levels
+    def _get_n_levels(self, data):
+        ns = np.array([], dtype=int)
         for random_effect in [["state", "district"], ["type"]]:
-            for j in range(len(random_effect), 0, -1):
-                curr_lvls = random_effect[:j]
-                combined = "beta_" + "_".join(
-                    [lvl + "_" + str(data_train[lvl].iloc[i]) for lvl in curr_lvls]
+            for i in range(len(random_effect), 0, -1):
+                curr_lvls = random_effect[:i]
+                lvl_map = self._factorize(curr_lvls, data)
+                lvl2num = lvl_map["lvl2num"]
+                ns = np.append(ns, len(lvl2num))
+        return ns
+
+    # define mapping
+    def _get_mapping(self, data):
+        mapping = np.empty((0, data.shape[0]), dtype=int)
+        for random_effect in [["state", "district"], ["type"]]:
+            for i in range(len(random_effect), 0, -1):
+                curr_lvls = random_effect[:i]
+                lvl_map = self._factorize(curr_lvls, data)
+                lvl2num = lvl_map["lvl2num"]
+                combined = (
+                    data[curr_lvls]
+                    .apply(lambda row: "____".join(row.values.astype(str)), axis=1)
+                    .values
                 )
-                if combined not in re_map_to_stan_params:
-                    re_map_to_stan_params[combined] = (ranges[re_q_position][0]) + (
-                        mapping[i][re_q_position] - 1
+                curr_map = np.array([lvl2num[x] for x in combined])
+                mapping = np.append(mapping, [curr_map], axis=0)
+
+        return mapping.T
+
+    # define ranges
+    def _get_ranges(self, data):
+        n_levels = self._get_n_levels(data)
+        cumsum_ns = np.cumsum(n_levels, dtype=int)
+        tmp = np.concatenate((cumsum_ns, np.array([0], dtype=int)))
+        start = np.concatenate(
+            (np.array([1], dtype=int), 1 + cumsum_ns[:-1], np.array([0], dtype=int))
+        )
+        ranges = np.array((start, tmp)).T
+        return ranges
+
+    def obtain_posterior(self, data_train, args_dict, model=None):
+        """
+      Stan impmementation of n-school model.
+
+      Inputs:
+      - data_train
+      - args_dict: a dict of model arguments
+      Returns:
+      - samples_stan(dict): posterior samples of all parameters
+      - timing_info(dict): compile_time, inference_time
+      """
+        global CODE
+
+        # mapping
+        mapping = self._get_mapping(data_train)
+
+        # ranges
+        ranges = self._get_ranges(data_train)
+
+        # n_levels
+        n_levels = self._get_n_levels(data_train)
+
+        # define sum_ns
+        sum_ns = np.sum(n_levels)
+
+        # generating stan mapping to state/district/type values!
+        re_map_to_stan_params = {}
+        for i in range(data_train.shape[0]):
+            re_q_position = 0
+            for random_effect in [["state", "district"], ["type"]]:
+                for j in range(len(random_effect), 0, -1):
+                    curr_lvls = random_effect[:j]
+                    combined = "beta_" + "_".join(
+                        [lvl + "_" + str(data_train[lvl].iloc[i]) for lvl in curr_lvls]
                     )
-                re_q_position += 1
+                    if combined not in re_map_to_stan_params:
+                        re_map_to_stan_params[combined] = (ranges[re_q_position][0]) + (
+                            mapping[i][re_q_position] - 1
+                        )
+                    re_q_position += 1
 
-    data_stan = {
-        "n": data_train.shape[0],
-        "p": 1,
-        "q": 3,
-        "yi": data_train["yi"].values,
-        "sei": data_train["sei"].values,
-        "X": np.ones(shape=data_train.shape[0])[..., None],
-        "ranges": ranges,
-        "sum_ns": sum_ns,
-        "mapping": mapping,
-        "sd_tau": 1.0,
-    }
-
-    # compile the model, time it
-    start_time = time.time()
-    model = pystan.StanModel(model_code=CODE, model_name="n_schools")
-    elapsed_time_compile_stan = time.time() - start_time
-
-    if args_dict["inference_type"] == "vi":
-        raise Exception(f"VI is not supported for the N_schools model.")
-
-    elif args_dict["inference_type"] == "mcmc":
-        # sample the parameter posteriors, time it
-        start_time = time.time()
-        fit = model.sampling(
-            data=data_stan,
-            iter=int(args_dict["num_samples_stan"]),
-            chains=1,
-            check_hmc_diagnostics=False,
-        )
-        samples_stan = fit.to_dataframe(
-            pars=["beta_c", "sds", "u"],
-            permuted=False,
-            inc_warmup=True,
-            diagnostics=False,
-        )
-        elapsed_time_sample_stan = time.time() - start_time
-
-        # rename columns to be similar to BMG n_school
-        samples_stan.rename(
-            columns={f"u[{v}]": k for k, v in re_map_to_stan_params.items()},
-            inplace=True,
-        )
-
-        samples_stan.rename(columns={"beta_c[1]": "beta_0"}, inplace=True)
-
-        timing_info = {
-            "compile_time": elapsed_time_compile_stan,
-            "inference_time": elapsed_time_sample_stan,
+        data_stan = {
+            "n": data_train.shape[0],
+            "p": 1,
+            "q": 3,
+            "yi": data_train["yi"].values,
+            "sei": [i.item() for i in data_train["sei"].values],
+            "X": np.ones(shape=data_train.shape[0])[..., None],
+            "ranges": ranges,
+            "sum_ns": sum_ns,
+            "mapping": mapping,
+            "sd_tau": 1.0,
         }
 
-        return (samples_stan, timing_info)
+        # compile the model, time it
+        start_time = time.time()
+        model = pystan.StanModel(model_code=CODE, model_name="n_schools")
+        elapsed_time_compile_stan = time.time() - start_time
+
+        if args_dict["inference_type"] == "vi":
+            raise Exception(f"VI is not supported for the N_schools model.")
+
+        elif args_dict["inference_type"] == "mcmc":
+            # sample the parameter posteriors, time it
+            start_time = time.time()
+            fit = model.sampling(
+                data=data_stan,
+                iter=int(args_dict["num_samples_stan"]),
+                chains=1,
+                check_hmc_diagnostics=False,
+            )
+            samples_stan = fit.to_dataframe(
+                pars=["beta_c", "sds", "u"],
+                permuted=False,
+                inc_warmup=True,
+                diagnostics=False,
+            )
+            elapsed_time_sample_stan = time.time() - start_time
+
+            # rename columns to be similar to BMG n_school
+            samples_stan.rename(
+                columns={f"u[{v}]": k for k, v in re_map_to_stan_params.items()},
+                inplace=True,
+            )
+
+            samples_stan.rename(columns={"beta_c[1]": "beta_0"}, inplace=True)
+
+            timing_info = {
+                "compile_time": elapsed_time_compile_stan,
+                "inference_time": elapsed_time_sample_stan,
+            }
+
+            return (samples_stan, timing_info)
