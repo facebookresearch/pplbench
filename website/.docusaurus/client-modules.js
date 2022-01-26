f25f9011800dfdbf07cd594423b1d04ba6b6e671
Xiaoyan Wang <xiaoyan0@fb.com>
2022-01-25 17:11:31 -0800

    Enable License Lint for Bean Machine and PPL Bench (#1316)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/beanmachine/pull/1316
    
    This diff enable license lint for Bean Machine and PPL Bench, which will enforce a copyright statement on top of every file under PPL Team's codebase. This will also give us a diff-time warning so we can avoid accidentally committing codes without the license and failing OSS requirement (such as T110683359).
    
    I was hoping to enable this linter for a while so we don't have to keeping track of the headers manually, but license lint was using the "Facebook" variant of the headers. Now that it's updated to use "Meta," we can safely enable it for our project without having to do a major codemod later on :).
    
    In addition to enabling the linter, this diff also updates the few files that failed the license requirement. These have to be done in the same diff to make the linter happy.
    
    Reviewed By: neerajprad
    
    Differential Revision: D33772236
    
    fbshipit-source-id: ed6d7a7c7d329e471c99e2e5dc27462889225f32

--- website/.docusaurus/client-modules.js
+++ website/.docusaurus/client-modules.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 export default [
   require("/Users/dvinnik/work/ppl/pplbench/website/node_modules/remark-admonitions/styles/infima.css"),
   require("/Users/dvinnik/work/ppl/pplbench/website/node_modules/remark-admonitions/styles/infima.css"),
